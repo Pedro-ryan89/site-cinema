a1664837a88098189b2ada0b5274de5c5b58a38f
Vectorgg15 <142703859+Vectorgg15@users.noreply.github.com>
2023-11-06 17:26:53 -0300

    Update navegacao.js

--- site_cinema/js/navegacao.js
+++ site_cinema/js/navegacao.js
@@ -1,98 +1,106 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Obtém as imagens clicáveis
-    const fnafImage = document.querySelector(".filmes a[href='fnaf.html']");
-    const oppenheimerImage = document.querySelector(".filmes img[src='/img/filmes_templates/oppenheimer_39761_home.jpg']");
-    const patrulhaCaninaImage = document.querySelector(".filmes img[src='/img/filmes_templates/patrulha-canina-um-filme-superpoderoso_43279_home.jpg']");
-    const exorcistaImage = document.querySelector(".filmes img[src='/img/filmes_templates/o-exorcista-o-devoto_46537_home.jpg']");
-  
-    // Função para criar uma página de filme
-    function createFilmePage(titulo, genero, duracao, classificacao, imgSrc, trailerLink) {
-        const main = document.querySelector("main");
-        main.innerHTML = ""; // Limpa o conteúdo atual do elemento main
-    
-        // Cria um link para o arquivo CSS compartilhado
+    const filmes = [
+        {
+            titulo: "Five Nights at Freddy's - O Pesadelo Sem Fim",
+            genero: "Terror",
+            duracao: "01h49",
+            classificacao: "14",
+            imgSrc: "/img/filmes_templates/five-nights-at-freddys-o-pesadelo-sem-fim_46543_home.jpg",
+            linkTrailer: "https://www.youtube.com/watch?v=h2lVX71L_3A&t=1s"
+        },
+        {
+            titulo: "Oppenheimer",
+            genero: "Drama",
+            duracao: "3h00",
+            classificacao: "16",
+            imgSrc: "/img/filmes_templates/oppenheimer_39761_home.jpg",
+            linkTrailer: "https://www.youtube.com/watch?v=F3OxA9Cz17A"
+        },
+        {
+            titulo: "Patrulha Canina: Um Filme Superpoderoso",
+            genero: "Animação",
+            duracao: "1h35",
+            classificacao: "L",
+            imgSrc: "/img/filmes_templates/patrulha-canina-um-filme-superpoderoso_43279_home.jpg",
+            linkTrailer: "https://www.youtube.com/watch?v=GviDlxgG38M"
+        },
+        {
+            titulo: "O Exorcista - O Devoto",
+            genero: "Terror",
+            duracao: "2h01",
+            classificacao: "16",
+            imgSrc: "/img/filmes_templates/o-exorcista-o-devoto_46537_home.jpg",
+            linkTrailer: "https://www.youtube.com/watch?v=96RCGOaNuCM"
+        },
+        {
+            titulo: "Trolls 3 - Juntos Novamente",
+            genero: "Animação",
+            duracao: "1h32",
+            classificacao: "L",
+            imgSrc: "/img/filmes_templates/trolls-3-juntos-novamente_40025_home.jpg",
+            linkTrailer: "https://youtu.be/E1pkNoojLus"
+        },
+        {
+            titulo: "Mussum, o filmis",
+            genero: "Comédia",
+            duracao: "2h02",
+            classificacao: "12",
+            imgSrc: "/img/filmes_templates/mussum-o-filmis_46539_home.jpg",
+            linkTrailer: ""
+        },
+        {
+          titulo: "As Marvels",
+          genero: "Ação",
+          duracao: "1h45",
+          classificacao: "L",
+          imgSrc: "/img/filmes_templates/as-marvels_42247_home.jpg",
+          linkTrailer: ""
+        }
+    ];
+
+    const main = document.querySelector("main");
+
+    function criarPaginaFilme(filme) {
+        main.innerHTML = "";
+
         const link = document.createElement("link");
         link.rel = "stylesheet";
         link.type = "text/css";
-        link.href = "/css/style.css"; // Nome do arquivo CSS compartilhado
+        link.href = "/css/style.css";
         document.head.appendChild(link);
-    
+
         const h1 = document.createElement("h1");
-        h1.textContent = titulo;
-    
+        h1.textContent = filme.titulo;
+
         const section = document.createElement("section");
         section.className = "filme";
-        section.innerHTML = `
-          <img src="${imgSrc}" alt="">
-          <ul>
-            <li>Gênero: <b>${genero}</b></li>
-            <li>Duração: <b>${duracao}</b></li>
-            <li>Classificação: <a class="classificacao-${classificacao}">${classificacao}</a></li>
-            <li><a class="trailer-button btn-${classificacao}" href="${trailerLink}">Assistir ao Trailer</a></li>
-          </ul>
-    `;
 
-    main.appendChild(h1);
-    main.appendChild(section);
-  }
-  
-  fnafImage.addEventListener("click", function(event) {
-    event.preventDefault(); // Impede o comportamento padrão do link
-    createFilmePage(
-      "Five Nights at Freddy's - O Pesadelo Sem Fim",
-      "Terror",
-      "01h49",
-      "14",
-      "/img/filmes_templates/five-nights-at-freddys-o-pesadelo-sem-fim_46543_home.jpg",
-      "https://www.youtube.com/watch?v=h2lVX71L_3A&t=1s" // Link para o trailer de Five Nights at Freddy's
-    );
-  });
-  
-    oppenheimerImage.addEventListener("mouseover", function() {
-      oppenheimerImage.style.cursor = "pointer"; // Altera o cursor para indicar que é clicável
-    });
-  
-    oppenheimerImage.addEventListener("click", function(event) {
-      event.preventDefault(); // Impede o comportamento padrão do link
-      createFilmePage(
-        "Oppenheimer",
-        "Drama",
-        "3h00",
-        "16",
-        "/img/filmes_templates/oppenheimer_39761_home.jpg",
-        "https://www.youtube.com/watch?v=F3OxA9Cz17A" // Link para o trailer de Oppenheimer
-      );
-    });
-  
-    patrulhaCaninaImage.addEventListener("mouseover", function() {
-      patrulhaCaninaImage.style.cursor = "pointer"; // Altera o cursor para indicar que é clicável
-    });
-  
-    patrulhaCaninaImage.addEventListener("click", function(event) {
-      event.preventDefault(); // Impede o comportamento padrão do link
-      createFilmePage(
-        "Patrulha Canina: Um Filme Superpoderoso",
-        "Animação",
-        "1h35",
-        "L",
-        "/img/filmes_templates/patrulha-canina-um-filme-superpoderoso_43279_home.jpg",
-        "https://www.youtube.com/watch?v=GviDlxgG38M" // Link para o trailer de Patrulha Canina
-      );
-    });
-  
-    exorcistaImage.addEventListener("mouseover", function() {
-      exorcistaImage.style.cursor = "pointer"; // Altera o cursor para indicar que é clicável
-    });
-  
-    exorcistaImage.addEventListener("click", function(event) {
-      event.preventDefault(); // Impede o comportamento padrão do link
-      createFilmePage(
-        "O Exorcista - O devoto",
-        "Terror",
-        "2h01",
-        "16",
-        "/img/filmes_templates/o-exorcista-o-devoto_46537_home.jpg",
-        "https://www.youtube.com/watch?v=96RCGOaNuCM" // Link para o trailer de O Exorcista
-      );
-    });
-  });
+        section.innerHTML = `
+          <img src="${filme.imgSrc}" alt="">
+          <ul>
+            <li>Gênero: <b>${filme.genero}</b></li>
+            <li>Duração: <b>${filme.duracao}</b></li>
+            <li>Classificação: <a class="classificacao-${filme.classificacao}">${filme.classificacao}</a></li>
+            <li><a class="trailer-button btn-${filme.classificacao}" href="${filme.linkTrailer}" target="_blank">Assistir ao Trailer</a></li>
+          </ul>
+        `;
+
+        main.appendChild(h1);
+        main.appendChild(section);
+    }
+
+    // Adicione eventos semelhantes para as outras imagens de filmes
+    for (let i = 0; i < filmes.length; i++) {
+        const imagem = document.querySelector(`.filmes img[src='${filmes[i].imgSrc}']`);
+        imagem.addEventListener("mouseover", function() {
+            imagem.style.cursor = "pointer";
+        });
+
+        imagem.addEventListener("click", function(event) {
+            const carrossel = document.querySelector(".carrossel");
+            carrossel.style.display = "none";
+            event.preventDefault();
+            criarPaginaFilme(filmes[i]);
+        });
+    }
+});
