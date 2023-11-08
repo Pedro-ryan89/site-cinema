4d6603c12a35b362603fc584d4e7733370bb2376
Vectorgg15 <142703859+Vectorgg15@users.noreply.github.com>
2023-11-08 00:36:42 -0300

    Update filme.js

--- site_cinema/js/filme.js
+++ site_cinema/js/filme.js
@@ -1,5 +1,3 @@
-// filme.js (para filme.html)
-
 document.addEventListener("DOMContentLoaded", function() {
     const filmeInfoSection = document.querySelector(".filme-info");
 
@@ -57,12 +55,12 @@ document.addEventListener("DOMContentLoaded", function() {
                 linkTrailer: ""
             },
             {
-              titulo: "As Marvels",
-              genero: "Ação",
-              duracao: "1h45",
-              classificacao: "L",
-              imgSrc: "/img/filmes_templates/as-marvels_42247_home.jpg",
-              linkTrailer: ""
+                titulo: "As Marvels",
+                genero: "Ação",
+                duracao: "1h45",
+                classificacao: "L",
+                imgSrc: "/img/filmes_templates/as-marvels_42247_home.jpg",
+                linkTrailer: ""
             },
             {
                 titulo: "Jogos Vorazes – A Cantiga dos Pássaros e das Serpentes",
@@ -71,7 +69,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 classificacao: "14",
                 imgSrc: "/img/filmes_templates/jogos-vorazes-a-cantiga-dos-passaros-e-das-serpentes_28791_home.jpg",
                 linkTrailer: ""
-              }
+            }
         ];
 
         if (filmes[filmeIndex]) {
@@ -81,12 +79,134 @@ document.addEventListener("DOMContentLoaded", function() {
             document.getElementById("filme-genero").textContent = filme.genero;
             document.getElementById("filme-duracao").textContent = filme.duracao;
 
-            // Adicione a classe CSS com base na classificação
             const classificacaoElement = document.getElementById("filme-classificacao");
             classificacaoElement.textContent = filme.classificacao;
             classificacaoElement.classList.add(`classificacao-${filme.classificacao}`);
 
             document.getElementById("filme-trailer").href = filme.linkTrailer;
+
+            const filmePersonalizado = {
+                "0": {
+                    dia01: {
+                        h4: "Sala 01",
+                        h5: "Dublado"
+                    },
+                    dia02: {
+                        h4: "Sala 02",
+                        h5: "Legendado"
+                    },
+                    dia03: {
+                        h4: "Sala 03",
+                        h5: "Dublado"
+                    },
+                    dia04: {
+                        h4: "Sala 04",
+                        h5: "Legendado"
+                    },
+                    dia05: {
+                        h4: "Sala 04",
+                        h5: "Legendado"
+                    },
+                    dia06: {
+                        h4: "Sala 04",
+                        h5: "Legendado"
+                    }
+                },
+                "1": {
+                    dia01: {
+                        h4: "Sala 01",
+                        h5: "Dublado"
+                    },
+                    dia02: {
+                        h4: "Sala 02",
+                        h5: "Legendado"
+                    },
+                    dia03: {
+                        h4: "Sala 03",
+                        h5: "Dublado"
+                    },
+                    dia04: {
+                        h4: "Sala 04",
+                        h5: "Legendado"
+                    },
+                    dia05: {
+                        h4: "Sala 04",
+                        h5: "Legendado"
+                    },
+                    dia06: {
+                        h4: "Sala 04",
+                        h5: "Legendado"
+                    }
+                },
+                // ... (mantenha as outras configurações de filmes personalizadas)
+            };
+
+            if (filmePersonalizado.hasOwnProperty(filmeIndex)) {
+                const infoPersonalizada = filmePersonalizado[filmeIndex];
+
+                for (let i = 1; i <= 6; i++) {
+                    const diaInfo = infoPersonalizada[`dia0${i}`];
+                    const diaHorariosSection = document.getElementById(`sala-0${i}-ingressos`);
+
+                    diaHorariosSection.querySelector("h4").textContent = diaInfo.h4;
+                    diaHorariosSection.querySelector("h5").textContent = diaInfo.h5;
+                }
+            }
+
+            const horarios = {
+                "0": {
+                    dia01: ["15:20", "17:40", "20:10"],
+                    dia02: ["14:30", "16:50", "19:20"],
+                    dia03: ["16:00", "18:20"],
+                    dia04: ["13:45", "15:45", "19:45"],
+                    dia05: ["13:45", "16:45"],
+                    dia06: ["13:45", "16:45"]
+                },
+                "1": {
+                    dia01: ["17:30", "20:00"],
+                    dia02: ["16:45", "19:15"],
+                    dia03: ["15:00", "17:30"],
+                    dia04: ["14:15", "16:15", "18:45"],
+                    dia05: ["13:45", "19:45"],
+                    dia06: ["13:45", "19:45"]
+                },
+                // ... (mantenha as outras configurações de horários)
+            };
+
+            if (horarios.hasOwnProperty(filmeIndex)) {
+                for (let i = 1; i <= 6; i++) {
+                    const horarioButtons = horarios[filmeIndex][`dia0${i}`];
+                    const diaHorariosSection = document.getElementById(`sala-0${i}-ingressos`);
+                    const horariosSection = diaHorariosSection.querySelector(".horarios");
+
+                    horarioButtons.forEach(horario => {
+                        const button = document.createElement("button");
+                        button.textContent = horario;
+                        horariosSection.appendChild(button);
+                    });
+                }
+            }
         }
     }
+
+    const buttons = document.querySelectorAll('.botoes-dias button');
+    const ingressosSections = document.querySelectorAll('.ingressos');
+
+    buttons.forEach((button, index) => {
+        button.addEventListener('click', () => {
+            buttons.forEach(btn => {
+                btn.classList.remove('ativo');
+            });
+
+            button.classList.add('ativo');
+
+            ingressosSections.forEach((section, i) => {
+                if (i === index) {
+                    section.style.display = 'block';
+                } else {
+                    section.style.display = 'none';
+                }
+            });
+        });
+    });
 });
