4f255d0f312e0c461b279ddcec465540c5116563
Pedro Ryan <120953398+Pedro-ryan89@users.noreply.github.com>
2023-11-20 16:27:19 -0300

    Update filme.js

--- site_cinema/js/filme.js
+++ site_cinema/js/filme.js
@@ -71,6 +71,7 @@ function configurarHorariosDia(horarios, i, diaHorariosSection) {
             const button = criarBotaoHorario(horario);
             horariosSection.appendChild(button);
         });
+
     }
 }
 
@@ -78,6 +79,12 @@ function obterHorariosPersonalizados(filmeIndex) {
     const filmePersonalizado = {
         "0": { dia01: { h4: "Sala 01", h5: "Dublado" }, dia02: { h4: "Sala 02", h5: "Legendado" }, dia03: { h4: "Sala 03", h5: "Dublado" }, dia04: { h4: "Sala 04", h5: "Legendado" }, dia05: { h4: "Sala 04", h5: "Legendado" }, dia06: { h4: "Sala 04", h5: "Legendado" } },
         "1": { dia01: { h4: "Sala 01", h5: "Dublado" }, dia02: { h4: "Sala 02", h5: "Legendado" }, dia03: { h4: "Sala 03", h5: "Dublado" }, dia04: { h4: "Sala 04", h5: "Legendado" }, dia05: { h4: "Sala 04", h5: "Legendado" }, dia06: { h4: "Sala 04", h5: "Legendado" } },
+        "2": { dia01: { h4: "Sala 01", h5: "Dublado" }, dia02: { h4: "Sala 02", h5: "Legendado" }, dia03: { h4: "Sala 03", h5: "Dublado" }, dia04: { h4: "Sala 04", h5: "Legendado" }, dia05: { h4: "Sala 04", h5: "Legendado" }, dia06: { h4: "Sala 04", h5: "Legendado" } },
+        "3": { dia01: { h4: "Sala 01", h5: "Dublado" }, dia02: { h4: "Sala 02", h5: "Legendado" }, dia03: { h4: "Sala 03", h5: "Dublado" }, dia04: { h4: "Sala 04", h5: "Legendado" }, dia05: { h4: "Sala 04", h5: "Legendado" }, dia06: { h4: "Sala 04", h5: "Legendado" } },
+        "4": { dia01: { h4: "Sala 01", h5: "Dublado" }, dia02: { h4: "Sala 02", h5: "Legendado" }, dia03: { h4: "Sala 03", h5: "Dublado" }, dia04: { h4: "Sala 04", h5: "Legendado" }, dia05: { h4: "Sala 04", h5: "Legendado" }, dia06: { h4: "Sala 04", h5: "Legendado" } },
+        "5": { dia01: { h4: "Sala 01", h5: "Nacional" }, dia02: { h4: "Sala 02", h5: "Nacional" }, dia03: { h4: "Sala 03", h5: "Nacional" }, dia04: { h4: "Sala 04", h5: "Nacional" }, dia05: { h4: "Sala 04", h5: "Nacional" }, dia06: { h4: "Sala 04", h5: "Nacional" } },
+        "6": { dia01: { h4: "Sala 01", h5: "Dublado" }, dia02: { h4: "Sala 02", h5: "Legendado" }, dia03: { h4: "Sala 03", h5: "Dublado" }, dia04: { h4: "Sala 04", h5: "Legendado" }, dia05: { h4: "Sala 04", h5: "Legendado" }, dia06: { h4: "Sala 04", h5: "Legendado" } },
+        "7": { dia01: { h4: "Sala 01", h5: "Dublado" }, dia02: { h4: "Sala 02", h5: "Legendado" }, dia03: { h4: "Sala 03", h5: "Dublado" }, dia04: { h4: "Sala 04", h5: "Legendado" }, dia05: { h4: "Sala 04", h5: "Legendado" }, dia06: { h4: "Sala 04", h5: "Legendado" } }
     };
 
     return filmePersonalizado[filmeIndex];
@@ -87,6 +94,13 @@ function obterHorarios(filmeIndex) {
     const horarios = {
         "0": { dia01: ["15:20", "17:40", "20:10"], dia02: ["14:30", "16:50", "19:20"], dia03: ["16:00", "18:20"], dia04: ["13:45", "15:45", "19:45"], dia05: ["13:45", "16:45"], dia06: ["13:45", "16:45"] },
         "1": { dia01: ["17:30", "20:00"], dia02: ["16:45", "19:15"], dia03: ["15:00", "17:30"], dia04: ["14:15", "16:15", "18:45"], dia05: ["13:45", "19:45"], dia06: ["13:45", "19:45"] },
+        "2": { dia01: ["17:30", "20:00"], dia02: ["16:45", "19:15"], dia03: ["15:00", "17:30"], dia04: ["14:15", "16:15", "18:45"], dia05: ["13:45", "19:45"], dia06: ["13:45", "19:45"] },
+        "3": { dia01: ["17:30", "20:00"], dia02: ["16:45", "19:15"], dia03: ["15:00", "17:30"], dia04: ["14:15", "16:15", "18:45"], dia05: ["13:45", "19:45"], dia06: ["13:45", "19:45"] },
+        "4": { dia01: ["17:30", "20:00"], dia02: ["16:45", "19:15"], dia03: ["15:00", "17:30"], dia04: ["14:15", "16:15", "18:45"], dia05: ["13:45", "19:45"], dia06: ["13:45", "19:45"] },
+        "5": { dia01: ["17:30", "20:00"], dia02: ["16:45", "19:15"], dia03: ["15:00", "17:30"], dia04: ["14:15", "16:15", "18:45"], dia05: ["13:45", "19:45"], dia06: ["13:45", "19:45"] },
+        "6": { dia01: ["17:30", "20:00"], dia02: ["16:45", "19:15"], dia03: ["15:00", "17:30"], dia04: ["14:15", "16:15", "18:45"], dia05: ["13:45", "19:45"], dia06: ["13:45", "19:45"] },
+        "7": { dia01: ["17:30", "20:00"], dia02: ["16:45", "19:15"], dia03: ["15:00", "17:30"], dia04: ["14:15", "16:15", "18:45"], dia05: ["13:45", "19:45"], dia06: ["13:45", "19:45"] },
+
     };
 
     return horarios[filmeIndex];
@@ -138,5 +152,5 @@ function configurarBotoesHorarios() {
 }
 
 function redirecionarParaCompra() {
-    window.location.href = '/html/finalizar_compra.html';
+    window.location.href = '/hmtl/finalizar_compra.html';
 }
